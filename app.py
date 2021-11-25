6250aaf923e91259e0d5e52ca94b391bd3009a5b
Jéssica Avelar <89416633+jessicaavelar@users.noreply.github.com>
2021-11-25 11:49:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,23 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return """
-        <h1>Olá, mundo!</h1>
-        <h2><b>:D</b></h2>
+        <h1>
+            Olá, mundo!
+        </h1>
+        <a href="/sobre">Sobre este site</a>
+        <h2>
+            <b>:D</b>
+        </h2>
     """
 
 @app.route("/sobre")
 def sobre():
     return """
-        <h1>Sobre</h1>
-        <p>Este site foi criado por <b>Jéssica Avelar</b>.</p>
+        <h1>
+            Sobre
+        </h1>
+        <a href="/">Página Inicial</a>
+        <p>
+            Este site foi criado por <b>Jéssica Avelar</b>.
+        </p>
     """
