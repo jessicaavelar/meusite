e6b79e13bc45558e1058d6ad92627b85eb6d1657
Jéssica Avelar <89416633+jessicaavelar@users.noreply.github.com>
2021-11-25 10:41:27 -0300

    identando

--- app.py
+++ app.py
@@ -4,8 +4,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<h1>Olá,<b>mundo</b>!</h1><p>:D</p>"
+    return """
+        <h1>Olá, <b>mundo</b>!</h1>
+        <p><b>:D</b></p>
+    """
 
 @app.route("/sobre")
 def sobre():
-    return "<h1>Sobre</h1> <p>Este site foi criado por <b>Jéssica Avelar</b>.</p>"
+    return """
+        <h1>Sobre</h1>
+        <p>Este site foi criado por <b>Jéssica Avelar</b>.</p>
+    """
