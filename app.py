f7f99a2338477400d551789147520f5d6ee69fca
Jéssica Avelar <89416633+jessicaavelar@users.noreply.github.com>
2021-11-25 10:12:31 -0300

    criar segunda página

--- app.py
+++ app.py
@@ -4,4 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p> Olá, <b>mundo</b>! <b>:D</b></p>"
+    return "<h1> Olá, <b>mundo</b>!</h1><p><b>:D</b></p>"
+
+@app.route("/sobre)
+def sobre():
+    return "<h1>Sobre</h1> <p>Esse site foi criado por <b>Jéssica Avelar</b>.</p>"
