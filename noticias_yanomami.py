250260deff6eff0a5313ee6745dfacbfd326ae22
Jéssica Avelar <89416633+jessicaavelar@users.noreply.github.com>
2022-01-06 20:16:31 -0300

    Create noticias_yanomami.py

---
+++ noticias_yanomami.py
@@ -0,0 +1,23 @@
+import csv
+import requests
+from bs4 import BeautifulSoup as bs 
+
+def noticias_site():
+    site = "https://terrasindigenas.org.br/noticias/4016/TI/500/1"
+    r = requests.get(site)
+    html = bs(r.text, "html.parser")
+
+    lista = html.find("ul", {"id": "list-noticias"})
+    tag_a = lista.find_all("a")
+    
+    noticias_ti = []
+    for tag in tag_a:
+        texto = tag.text
+        titulo = texto.split("-")
+        url = "https://terrasindigenas.org.br" + tag.attrs["href"]
+        noticias_ti.append({
+            "data": titulo[0],
+            "titulo": titulo[1],
+            "link": url,
+        })
+    return noticias_ti
