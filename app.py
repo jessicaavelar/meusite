25db2b082ff7f2a702af3d98a15dbd0e5592173a
Jéssica Avelar <89416633+jessicaavelar@users.noreply.github.com>
2022-01-06 19:54:57 -0300

    separar função covid

--- app.py
+++ app.py
@@ -1,37 +1,11 @@
 import csv
-import datetime
-import io
 import requests
 import os
 from bs4 import BeautifulSoup as bs
 from flask import Flask, render_template, request
 
+from covid_pr import dados_covid_pr
 
-def dados_covid_pr():
-    hoje = datetime.datetime.now().date()
-    ontem = hoje - datetime.timedelta(days=1)
-    conteudo = None
-    for data in (hoje, ontem):
-        url1 = f"https://www.saude.pr.gov.br/sites/default/arquivos_restritos/files/documento/{data.year}-{data.month:02d}/INFORME_EPIDEMIOLOGICO_{data.day:02d}_{data.month:02d}_{data.year}_OBITOS_CASOS_Municipio.csv"
-        url2 = url1.lower()
-        for url in (url1, url2):
-            resposta = requests.get(url)
-            if resposta.ok:
-                conteudo = resposta.content.decode(resposta.apparent_encoding)
-                break
-        if conteudo:
-            break
-    if not conteudo:
-        return None, None, None
-    casos = 0
-    obitos = 0
-    leitor = csv.DictReader(io.StringIO(conteudo), delimiter=";")
-    for registro in leitor:
-        casos += int(registro["Casos"])
-        obitos += int(registro["Obitos"])
-
-    return data, casos, obitos
-    
     
 def noticias_site():
     site = "https://terrasindigenas.org.br/noticias/4016/TI/500/1"
