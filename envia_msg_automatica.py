966c0f395a80c235f0c6b8a094877e58bf135535
Jéssica Avelar <89416633+jessicaavelar@users.noreply.github.com>
2022-01-06 20:25:19 -0300

    Update envia_msg_automatica.py

--- envia_msg_automatica.py
+++ envia_msg_automatica.py
@@ -10,4 +10,4 @@ url = f"https://api.telegram.org/bot{token}/sendMessage"
 chat_id = id = 5023867801
 texto = f"Olá!\nAcabei de capturar dados de COVID-19 do Paraná para a data {data}.\nCasos: {casos}\nÓbitos: {obitos}"
 mensagem = {"chat_id": chat_id, "text": texto }
-requests.post(url, data = menssagem)
+requests.post(url, data = mensagem)
